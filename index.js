c0cb73717250bfee81c95cee3a11073fa4b9935b
Rodolfo Gomez Sirimarco <im@rodolfo.gs>
2022-05-22 12:38:27 -0500

    Response errors as an object instead as a string

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ var amazon;
 function handlePromisedFunctionCb(resolve, reject) {
     return function _handlePromisedCallback(error, response) {
         if (error) {
-            var errorData = { error: error, status: null, message: null };
+            var errorData = { error: error, status: null, message: null, response: null };
             if (response !== null && typeof response === 'object') {
                 errorData.status = response.status;
                 errorData.message = response.message;
             }
-            return reject(JSON.stringify(errorData), response);
+            return reject(errorData);
         }
         return resolve(response);
     };
